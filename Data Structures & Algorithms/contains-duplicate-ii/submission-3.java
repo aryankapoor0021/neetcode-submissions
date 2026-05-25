4c434451bcea2aa06fc3a8e4ce0819676081c222
Aryan Kapoor <63351353+aryankapoor0021@users.noreply.github.com>
2026-05-26 00:35:21 +0530

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] a, int k) {
+        
+        HashSet<Integer> window=new HashSet<>();
+        int l=0;
+        for(int r=0;r<a.length;r++){
+            if(r-l>k){
+                window.remove(a[l]);
+                l++;
+            }
+            if(window.contains(a[r])) return true;
+        
+            window.add(a[r]);
+        }
+        return false;
+
+    }
+}
